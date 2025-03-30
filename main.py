f41a3b30454b9f756c2a8a4b39c1c4c3990667f1
Colorless <augustas.petraska@gmail.com>
2025-03-30 14:21:12 +0300

    Added a main.py file which will store all the algorithms

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('First Commit!')
-print('sasalink')
+from GUI import TradingApp
+import ttkbootstrap as tb
+import functions
+
+print(functions.NASDAQ_TO_NAME('AAPL'))
+
+root = tb.Window(themename="darkly")
+app = TradingApp(root)
+root.mainloop()
